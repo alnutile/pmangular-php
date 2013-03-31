233aa2acee98dcce271b1f86d99723052112a507
Alfred Nutile <al@freshjones.com>
2013-03-31 09:30:46 -0400

    Clean up for testing

--- app/js/services.js
+++ app/js/services.js
@@ -35,5 +35,23 @@ angular.module('myApp.services', ['ngResource']).
 			}
 
 		}
+	}]).
+	//The factory returns objects / functions that can be used by the controllers
+    factory('Hosting', ['$resource', '$http', '$rootScope', function($resource, $http, $rootScope){
+        return {
+        	//the resource provider interacting with the PHP backend
+	        api: 
+				$resource('api/v3/client/:clientId', {}, {
+			        update: {method:'PUT'}
+				}),
+			/*
+			* A generic function that can be called to emit an event on one ctrl that can be handled by another ctrl.
+			* The WineDetailCtrl emits it on each save, update or delete request, so that the WineListCtrl can react and update itself.	
+			*/
+			broadcastChange: function(){
+				$rootScope.$broadcast('handleBroadcast');
+			}
+
+		}
 	}])
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,31 +1,12 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
+describe('PM Controllers', function() {
+  describe('PeopleList', function(){
+    it('should create people list to click on', function() {
+      var scope = {};
+        ctrl = new PeopleList(scope);
+        expect(scope.phones.length).toBeGreaterThan(1);
+    }); 
+  )};
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
-
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
 });
